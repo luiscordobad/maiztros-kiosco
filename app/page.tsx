f4343dd7ff92c8860919317ae0c53f6468866d46
luiscordobad <luiscordobad@gmail.com>
2026-04-05 11:15:57 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-import { prisma } from '../lib/prisma';
-import KioscoClient from '../components/KioscoClient';
+import { prisma } from '@/lib/prisma';
+import KioscoClient from '@/components/KioscoClient';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
+  // SOLO TRAEMOS LO QUE ESTÁ EN STOCK
   const products = await prisma.product.findMany({
+    where: { isAvailable: true },
     orderBy: { basePrice: 'asc' }
   });
   
-  // Novedad: Traemos los modificadores (toppings)
-  const modifiers = await prisma.modifier.findMany();
+  const modifiers = await prisma.modifier.findMany({
+    where: { isAvailable: true }
+  });
 
   return <KioscoClient products={products} modifiers={modifiers} />;
 }
